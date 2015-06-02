1fdaa32df48b724d0853495bd783f9fb43a40758
mcclure15 <m001010111@gmail.com>
2015-06-01 21:35:33 -0700

    add pg file

---
+++ PokemonPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Pokemon {
+  var name: String = "name"
+  var type: String = "type"
+  var health: Int = 0
+  var attack: Int = 0
+  var defense: Int = 0
+  var speed: Int = 0
+}
+
